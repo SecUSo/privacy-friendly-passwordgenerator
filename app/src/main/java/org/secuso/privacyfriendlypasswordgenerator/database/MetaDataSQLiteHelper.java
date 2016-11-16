3685e1eb0cff9db0d25c43a1233cc8957b497328
Karola Marky <yonjuni@pinyto.de>
2016-11-16 16:53:33 +0200

    Username, upper/lower case added to DB

--- app/src/main/java/org/secuso/privacyfriendlypasswordgenerator/database/MetaDataSQLiteHelper.java
+++ app/src/main/java/org/secuso/privacyfriendlypasswordgenerator/database/MetaDataSQLiteHelper.java
@@ -25,10 +25,12 @@ public class MetaDataSQLiteHelper extends SQLiteOpenHelper {
 
     private static final String KEY_ID = "id";
     private static final String KEY_DOMAIN = "domain";
+    private static final String KEY_USERNAME = "username";
     private static final String KEY_LENGTH = "length";
     private static final String KEY_HAS_NUMBERS = "hasNumbers";
     private static final String KEY_HAS_SYMBOLS = "hasSymbols";
-    private static final String KEY_HAS_LETTERS = "hasLetters";
+    private static final String KEY_HAS_LETTERS_UP = "hasLettersUp";
+    private static final String KEY_HAS_LETTERS_LOW = "hasLettersLow";
 
     private static final String ITERATION = "interation";
 
@@ -42,10 +44,12 @@ public class MetaDataSQLiteHelper extends SQLiteOpenHelper {
                 "(" +
                 KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
                 KEY_DOMAIN + " TEXT NOT NULL," +
+                KEY_USERNAME + " TEXT NOT NULL," +
                 KEY_LENGTH + " INTEGER," +
                 KEY_HAS_NUMBERS + " INTEGER," +
                 KEY_HAS_SYMBOLS + " INTEGER," +
-                KEY_HAS_LETTERS + " INTEGER," +
+                KEY_HAS_LETTERS_UP + " INTEGER," +
+                KEY_HAS_LETTERS_LOW + " INTEGER," +
                 ITERATION + " INTEGER);";
 
         sqLiteDatabase.execSQL(CREATE_METADATA_TABLE);
@@ -63,10 +67,12 @@ public class MetaDataSQLiteHelper extends SQLiteOpenHelper {
 
         ContentValues values = new ContentValues();
         values.put(KEY_DOMAIN, metaData.getDOMAIN());
+        values.put(KEY_USERNAME, metaData.getUSERNAME());
         values.put(KEY_LENGTH, metaData.getLENGTH());
         values.put(KEY_HAS_NUMBERS, metaData.getHAS_NUMBERS());
         values.put(KEY_HAS_SYMBOLS, metaData.getHAS_SYMBOLS());
-        values.put(KEY_HAS_LETTERS, metaData.getHAS_LETTERS());
+        values.put(KEY_HAS_LETTERS_UP, metaData.getHAS_LETTERS_UP());
+        values.put(KEY_HAS_LETTERS_LOW, metaData.getHAS_LETTERS_LOW());
         values.put(ITERATION, metaData.getITERATION());
 
         database.insert(TABLE_METADATA, null, values);
@@ -88,11 +94,13 @@ public class MetaDataSQLiteHelper extends SQLiteOpenHelper {
                 metaData = new MetaData();
                 metaData.setID(Integer.parseInt(cursor.getString(0)));
                 metaData.setDOMAIN(cursor.getString(1));
-                metaData.setLENGTH(Integer.parseInt(cursor.getString(2)));
-                metaData.setHAS_NUMBERS(Integer.parseInt(cursor.getString(3)));
-                metaData.setHAS_SYMBOLS(Integer.parseInt(cursor.getString(4)));
-                metaData.setHAS_LETTERS(Integer.parseInt(cursor.getString(5)));
-                metaData.setITERATION(Integer.parseInt(cursor.getString(6)));
+                metaData.setUSERNAME(cursor.getString(2));
+                metaData.setLENGTH(Integer.parseInt(cursor.getString(3)));
+                metaData.setHAS_NUMBERS(Integer.parseInt(cursor.getString(4)));
+                metaData.setHAS_SYMBOLS(Integer.parseInt(cursor.getString(5)));
+                metaData.setHAS_LETTERS_UP(Integer.parseInt(cursor.getString(6)));
+                metaData.setHAS_LETTERS_LOW(Integer.parseInt(cursor.getString(7)));
+                metaData.setITERATION(Integer.parseInt(cursor.getString(8)));
 
                 metaDataList.add(metaData);
             } while (cursor.moveToNext());
@@ -106,10 +114,12 @@ public class MetaDataSQLiteHelper extends SQLiteOpenHelper {
 
         ContentValues values = new ContentValues();
         values.put(KEY_DOMAIN, metaData.getDOMAIN());
+        values.put(KEY_USERNAME, metaData.getUSERNAME());
         values.put(KEY_LENGTH, metaData.getLENGTH());
         values.put(KEY_HAS_NUMBERS, metaData.getHAS_NUMBERS());
         values.put(KEY_HAS_SYMBOLS, metaData.getHAS_SYMBOLS());
-        values.put(KEY_HAS_LETTERS, metaData.getHAS_LETTERS());
+        values.put(KEY_HAS_LETTERS_UP, metaData.getHAS_LETTERS_UP());
+        values.put(KEY_HAS_LETTERS_LOW, metaData.getHAS_LETTERS_LOW());
         values.put(ITERATION, metaData.getITERATION());
 
         return database.update(TABLE_METADATA, values, KEY_ID + " = ?",
@@ -127,7 +137,7 @@ public class MetaDataSQLiteHelper extends SQLiteOpenHelper {
         SQLiteDatabase database = this.getWritableDatabase();
 
         Cursor cursor = database.query(TABLE_METADATA, new String[]{KEY_ID,
-                        KEY_DOMAIN, KEY_LENGTH, KEY_HAS_NUMBERS, KEY_HAS_SYMBOLS, KEY_HAS_LETTERS, ITERATION}, KEY_ID + "=?",
+                        KEY_DOMAIN, KEY_USERNAME, KEY_LENGTH, KEY_HAS_NUMBERS, KEY_HAS_SYMBOLS, KEY_HAS_LETTERS_UP, KEY_HAS_LETTERS_LOW, ITERATION}, KEY_ID + "=?",
                 new String[]{String.valueOf(id)}, null, null, null, null);
 
         MetaData metaData = new MetaData();
@@ -135,11 +145,13 @@ public class MetaDataSQLiteHelper extends SQLiteOpenHelper {
         if( cursor != null && cursor.moveToFirst() ){
             metaData.setID(Integer.parseInt(cursor.getString(0)));
             metaData.setDOMAIN(cursor.getString(1));
-            metaData.setLENGTH(Integer.parseInt(cursor.getString(2)));
-            metaData.setHAS_NUMBERS(Integer.parseInt(cursor.getString(3)));
-            metaData.setHAS_SYMBOLS(Integer.parseInt(cursor.getString(4)));
-            metaData.setHAS_LETTERS(Integer.parseInt(cursor.getString(5)));
-            metaData.setITERATION(Integer.parseInt(cursor.getString(6)));
+            metaData.setUSERNAME(cursor.getString(2));
+            metaData.setLENGTH(Integer.parseInt(cursor.getString(3)));
+            metaData.setHAS_NUMBERS(Integer.parseInt(cursor.getString(4)));
+            metaData.setHAS_SYMBOLS(Integer.parseInt(cursor.getString(5)));
+            metaData.setHAS_LETTERS_UP(Integer.parseInt(cursor.getString(6)));
+            metaData.setHAS_LETTERS_LOW(Integer.parseInt(cursor.getString(6)));
+            metaData.setITERATION(Integer.parseInt(cursor.getString(8)));
             cursor.close();
         }
 
