5ac05e015aae0e3d1fa6cbff7fbe4092736512e7
Karola Marky <yonjuni@pinyto.de>
2016-11-19 22:03:23 +0200

    Help added

---
+++ app/src/main/java/org/secuso/privacyfriendlypasswordgenerator/activities/TutorialActivity.java
@@ -0,0 +1,13 @@
+package org.secuso.privacyfriendlypasswordgenerator.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by karo on 19.11.16.
+ */
+
+public class TutorialActivity extends AppCompatActivity{
+
+
+
+}
