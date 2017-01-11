894801ed7de1f6ef4338a95363b225700cf5305e
Karola <yonjuni@pinyto.de>
2017-01-12 00:54:51 +0200

    Refactoring of PrefManager

--- app/src/main/java/org/secuso/privacyfriendlypasswordgenerator/tutorial/PrefManager.java
+++ app/src/main/java/org/secuso/privacyfriendlypasswordgenerator/tutorial/PrefManager.java
@@ -7,21 +7,21 @@ import android.content.SharedPreferences;
  * Class structure taken from tutorial at http://www.androidhive.info/2016/05/android-build-intro-slider-app/
  */
 public class PrefManager {
-    SharedPreferences pref;
-    SharedPreferences.Editor editor;
-    Context _context;
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+    private Context context;
 
     // shared pref mode
-    int PRIVATE_MODE = 0;
+    private int PRIVATE_MODE = 0;
 
     // Shared preferences file name
-    private static final String PREF_NAME = "androidhive-welcome";
+    private static final String PREF_NAME = "pfa-pw-generator";
 
     private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
 
     public PrefManager(Context context) {
-        this._context = context;
-        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        this.context = context;
+        pref = context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
         editor = pref.edit();
     }
 
