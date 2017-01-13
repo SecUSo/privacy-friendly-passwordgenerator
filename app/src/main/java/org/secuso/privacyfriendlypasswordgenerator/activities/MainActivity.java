3dc60682cb36c669754755b8d896b19246765d38
Karola <yonjuni@pinyto.de>
2017-01-13 23:42:32 +0200

    Optimization of imports

--- app/src/main/java/org/secuso/privacyfriendlypasswordgenerator/activities/MainActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlypasswordgenerator/activities/MainActivity.java
@@ -14,7 +14,6 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.WindowManager;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.widget.LinearLayout;
