e1700e7f1a1ea36debd346c7015053e0af8747c0
Karola Marky <yonjuni@pinyto.de>
2017-03-10 21:06:43 +0100

    Added exception in case of a null key

--- app/src/main/java/org/secuso/privacyfriendlypasswordgenerator/generator/PBKDF2.java
+++ app/src/main/java/org/secuso/privacyfriendlypasswordgenerator/generator/PBKDF2.java
@@ -1,18 +1,18 @@
 /**
  * This file is part of Privacy Friendly Password Generator.
-
- Privacy Friendly Password Generator is free software:
- you can redistribute it and/or modify it under the terms of the
- GNU General Public License as published by the Free Software Foundation,
- either version 3 of the License, or any later version.
-
- Privacy Friendly Password Generator is distributed in the hope
- that it will be useful, but WITHOUT ANY WARRANTY; without even
- the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- See the GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with Privacy Friendly Password Generator. If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * Privacy Friendly Password Generator is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or any later version.
+ * <p>
+ * Privacy Friendly Password Generator is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with Privacy Friendly Password Generator. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.secuso.privacyfriendlypasswordgenerator.generator;
@@ -26,15 +26,15 @@ import javax.crypto.spec.SecretKeySpec;
 
 /**
  * This class creates PBKDF2 with sha256.
- *
+ * <p>
  * Class taken from https://github.com/pinae/ctSESAM-android/
  * last access 1st November 2016
  */
 public class PBKDF2 {
 
-    private static byte[] shaHMAC(String hashFunction, byte[] key, byte[] password) {
+    private static byte[] shaHMAC(String hashFunction, byte[] key, byte[] password) throws NullKeyException {
         if (key.length == 0) {
-            key = new byte[] { 0x00 };
+            throw new NullKeyException("Key has a length of 0.");
         }
         try {
             Mac sha_HMAC = Mac.getInstance("Hmac" + hashFunction);
@@ -49,9 +49,8 @@ public class PBKDF2 {
         }
     }
 
-    private static byte[] F (String hashFunction, byte[] password, byte[] salt, int iterations, int i)
-    {
-        byte[] Si = new byte[salt.length+4];
+    private static byte[] F(String hashFunction, byte[] password, byte[] salt, int iterations, int i) {
+        byte[] Si = new byte[salt.length + 4];
         System.arraycopy(salt, 0, Si, 0, salt.length);
         byte[] iByteArray = ByteBuffer.allocate(4).putInt(i).array();
         System.arraycopy(iByteArray, 0, Si, salt.length, iByteArray.length);
@@ -70,18 +69,20 @@ public class PBKDF2 {
     /**
      * Pass "SHA256" or "SHA384" or "SHA512" as the parameter hashFunction.
      */
-    public static byte[] hmac (String hashFunction, byte[] hashString, byte[] salt, int iterations)
-    {
+    public static byte[] hmac(String hashFunction, byte[] hashString, byte[] salt, int iterations) {
         int dkLen = 64;
         int hLen = 64;
         switch (hashFunction) {
-            case "SHA256": dkLen = 32;
+            case "SHA256":
+                dkLen = 32;
                 hLen = 32;
                 break;
-            case "SHA384": dkLen = 48;
+            case "SHA384":
+                dkLen = 48;
                 hLen = 48;
                 break;
-            case "SHA512": dkLen = 64;
+            case "SHA512":
+                dkLen = 64;
                 hLen = 64;
                 break;
         }
@@ -91,11 +92,19 @@ public class PBKDF2 {
         for (int i = 1; i <= l; i++) {
             byte[] T = F(hashFunction, hashString, salt, iterations, i);
             for (int k = 0; k < T.length; k++) {
-                if (i-1+k < dk.length) {
-                    dk[i-1+k] = T[k];
+                if (i - 1 + k < dk.length) {
+                    dk[i - 1 + k] = T[k];
                 }
             }
         }
         return dk;
     }
+
+    public static class NullKeyException extends RuntimeException {
+
+        public NullKeyException(String message) {
+            super(message);
+        }
+    }
+
 }
